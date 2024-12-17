dff8d2be3254c155755569e9601bef073faa7367
mrSinghSatyam <satyamsinghrs972518@gmail.com>
2024-12-17 19:58:57 +0530

    some more update

--- Form.js
+++ Form.js
@@ -1,23 +1,24 @@
 //FeedBack Form
 let name = document.getElementById("name1").value
-document.getElementById("FF").textContent = "Feedback Form"
-
-document.getElementById("name1").textContent = "Name:"
-
-document.getElementById("email1").textContent = "Email:"
-
-document.getElementById("div1").textContent = "Division:"
-document.getElementById("op1").textContent = "A"
-document.getElementById("op2").textContent = "B"
-document.getElementById("op3").textContent = "C"
-document.getElementById("op4").textContent = "D"
-
-document.getElementById("fd").textContent = "Feedback:"
-
-document.getElementById("sub").textContent = "Submit"
 console.log(name)
- function show(){
-    document.getElementById("myP").textContent = `Thankyou for your Feedback ${name}`
- }
+// document.getElementById("FF").textContent = "Feedback Form"
+
+// document.getElementById("name1").textContent = "Name:"
+
+// document.getElementById("email1").textContent = "Email:"
+
+// document.getElementById("div1").textContent = "Division:"
+// document.getElementById("op1").textContent = "A"
+// document.getElementById("op2").textContent = "B"
+// document.getElementById("op3").textContent = "C"
+// document.getElementById("op4").textContent = "D"
+
+// document.getElementById("fd").textContent = "Feedback:"
+
+// document.getElementById("sub").textContent = "Submit"
+// console.log(name)
+//  function show(){
+//     document.getElementById("myP").textContent = `Thankyou for your Feedback ${name}`
+//  }
 
 
