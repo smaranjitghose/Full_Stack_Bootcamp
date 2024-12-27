f6d451bb55d630b99a356494b483fd56831f0e30
Vrushi14 <vrushi1214@gmail.com>
2024-12-27 12:51:48 +0530

    All file commited

---
+++ javascript/Feedback.js
@@ -0,0 +1,50 @@
+//console.log("Today is not soo cool.")
+//console.log("The sky is Sunny.")
+
+//console.error(" We have all 6 lectures.")
+//console.alert("This script seems to be fun.")
+
+
+//This is commit
+
+//document.getElementById("myH1").textContent = "Thermosteel Bottles"
+//document.getElementById("myP1").textContent = "Milton presents a distinctive range of thermosteel bottles crafted to elevate your daily drink experience. These insulated water bottles are double-walled and both the walls are made from 100% premium-quality steel, ensuring longevity and safety"
+
+//document.getElementById("myH2").textContent = "Thermoware Bottles"
+//document.getElementById("myP2").textContent = "Thermoware bottles are tailored specifically to maintain chilled water for around four hours. Available in an array of dimensions, forms, hues, and graphics catering to diverse personal styles, the bottles offer choices for various preferences."
+
+//let x;
+//x = 10;
+//let y;
+//y = "Full Stack Developer";
+//let z = true
+//console.log(typeof x,x)
+
+//let age = 22
+//let gpa = 9.2
+//let name = "Vrushi"
+//
+// 
+// console.log("My name is $ ")
+
+// let name = "Vrushangi Mistry"
+// let exp1 = "Android Developer at Accenture(2020-2022)"
+// let exp2 = "Senior Android Developer at Zignut(2022-present)"
+// document.getElementById("myH1").textContent = "Vrushangi Mistry"
+// document.getElementById("Experience").textContent = "Experience"
+// document.getElementById("exp1").textContent = "I have 2 Years of experience at Accenture as Android Developer.I have also attend several meetings with HR's of Infosys,Amazon,etc.(2020-2022)"
+// document.getElementById("exp2").textContent = "I am present working at Zignut as Senior Android Developer.(2022-present)"
+// document.getElementById("Project").textContent = "Projects"
+//prompt("Enter the Details")
+// let name = prompt("Enter your name")
+// console.log(name)
+// document.getElementById("myH1").textContent = `${name}`
+
+//name = document.getElementById("MyName")
+// document.getElementById("submit").onclick = 
+function show()
+{
+    document.getElementById("myH1").textContent = ("FeedBack Form")
+    let name = document.getElementById("MyName").value
+    let email = document.getElementById("myEmail").value
+}
