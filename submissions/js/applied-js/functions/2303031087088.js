72bec6266ba1a34581f09b9c275f1c5c3af6733b
hanivaghani <166581899+hanivaghani@users.noreply.github.com>
2024-12-31 13:00:10 +0530

    2303031087088 - Assignment 11

---
+++ submissions/js/applied-js/functions/2303031087088.js
@@ -0,0 +1,55 @@
+//Task 1: Using Array Methods
+//Write a function squareNumbers(arr) using map() and arrow functions
+
+function squareNumbers(arr) {
+    return arr.map(num => num **2)}
+let nums = [2,4,6,8]
+let Squared = squareNumbers(nums)
+console.log(Squared)
+//output [ 4, 16, 36, 64 ]
+
+//Task 2: Custom Filter Function
+//Create a function filterEvenNumbers(arr) using filter() and arrow functions
+
+function filterEvenNumbers(arr){
+    return arr.filter(num => num % 2 == 0)}
+let numb = [2,3,6,9]
+let even = filterEvenNumbers(numb)
+console.log(even)
+//output [ 2, 6 ]
+
+//Task 3: Sum of Positive Numbers
+//Write a function sumPositiveNumbers(arr) that takes an array of numbers and returns the sum of all positive numbers using filter() and reduce() with arrow functions
+
+function sumPositiveNumbers(arr) {
+    return arr.filter(num => num > 0).reduce((sum, num) => sum + num, 0);
+  }
+  let numbers = [-7, 8, 2, -9, 0, -2, 9];
+  let positiveSum = sumPositiveNumbers(numbers);
+  console.log(positiveSum);
+//output 19
+
+//Task 4: Transform Array of Objects
+//Write a function getNames(arr) that takes an array of objects where each object has a name property, and returns an array of just the names using map() and arrow functions
+
+function getNames(arr) {
+    return arr.map(obj => obj.name);
+  }
+  let people = [
+    { name: "Hani", age: 23 },
+    { name: "Nancy", age: 20 },
+    { name: "Janvi", age: 21 }``
+  ];
+  let names = getNames(people);
+  console.log(names); 
+ //output [ 'Hani', 'Nancy', 'Janvi' ]
+
+//Task 5: Find the Longest Word
+//Write a function findLongestWord(arr) that takes an array of strings and returns the longest word using reduce() and an arrow function
+
+function findLongestWord(arr) {
+    return arr.reduce((longest, word) => word.length > longest.length ? word : longest, "");}
+  let words = ["Laptop", "Phone", "Speaker", "Computer", "Keyboard"];
+  let longestWord = findLongestWord(words);
+  console.log(longestWord); 
+//output Computer
